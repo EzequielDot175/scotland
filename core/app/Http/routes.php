2c848a938f321e54b2b662a69bd600cc0252c2ef
ezequiel <ezequiel@dot175.com>
2015-11-17 12:26:40 -0700

    first commit

---
+++ core/app/Http/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+use App\User;
+
+
+
+Route::get('/', function(){
+
+	if (Auth::attempt(['email' => 'ezequiel@dot175.com', 'password' => '123']))
+	{
+	    // The user is active, not suspended, and exists.
+	    echo "logueado";
+	}
+
+});
+
+Route::get('other', function(){
+	var_dump(Auth::check());
+});
+
+// Route::get('home', 'HomeController@index');
+
+// Route::controllers([
+// 	'auth' => 'Auth\AuthController',
+// 	'password' => 'Auth\PasswordController',
+// ]);
